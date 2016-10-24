97d6061fa4ade5416a9a0b3cdb7ab2a96ca0f309
myklv <+@myklv.com>
2016-10-24 00:48:23 -0400

    simple prompt for lithp repl

--- lithp.c
+++ lithp.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+static char input_buffer[2048];
+
 int main(int argc, char** argv) {
-  puts("Hello, world!");
-  return 0;
+    puts("Lithp Version 0.0.0.0.0.0.0.1");
+    puts("Press Ctrl+c to Exit");
+
+    while (1) {
+        fputs("lispy> ", stdout);
+        fgets(input_buffer, 2048, stdin);
+        printf("No you're a %s", input_buffer);
+    }
+
+    return 0;
 }
